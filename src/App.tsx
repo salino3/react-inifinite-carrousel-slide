3f6e25c5e4cf63577cf01a13ddc24ac2e1b94609
Salino3 <flaviobcn012@gmail.com>
2025-03-22 10:42:11 +0100

    add photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.scss";
 
 function App() {
-  return <div className="rootApp"></div>;
+  return (
+    <div className="rootApp">
+      <h1>react-inifinite-carrousel-slide</h1>
+      <img src="/photo-1.png" alt="Photo 1" />
+    </div>
+  );
 }
 
 export default App;
