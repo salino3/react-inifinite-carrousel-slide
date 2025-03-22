7b68995057414d5e66865278b12bcd858f79167e
Salino3 <flaviobcn012@gmail.com>
2025-03-22 10:51:59 +0100

    changings

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="rootApp">
       <h1>react-inifinite-carrousel-slide</h1>
-      <img src="/photo-1.png" alt="Photo 1" />
+      <img src="assets/photo-1.png" alt="Photo 1" />
     </div>
   );
 }
